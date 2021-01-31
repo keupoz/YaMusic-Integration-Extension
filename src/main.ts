409dd0b61e724681a7541333032d9a2e7d820aec
Ivan Sokolov <keupoz@gmail.com>
2021-01-31 07:39:42 +0700

    Implement notifications

--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,47 @@ function generateArtwork(url: string): MediaImage[] {
     }));
 }
 
+let lastNotification: Notification;
+
 function updateTrack(track?: TrackInfo): void {
     if (navigator.mediaSession === undefined) throw new Error("MediaSession API isn't supported by your browser");
 
-    navigator.mediaSession.metadata = track ? new MediaMetadata({
-        title: track.title + (track.version ? `(${track.version})` : ""),
-        artist: track.artists.map((artist) => artist.title).join(", "),
+    if (!track) {
+        navigator.mediaSession.metadata = null;
+        return;
+    }
+
+    const title = track.title + (track.version ? ` (${track.version})` : ""),
+        artist = track.artists.map((artist) => artist.title).join(", "),
+        cover = track.cover ? generateArtwork(`https://${track.cover}`) : undefined;
+
+    navigator.mediaSession.metadata = new MediaMetadata({
+        title: title,
+        artist: artist || track.album?.title,
         album: track.album?.title,
-        artwork: track.cover ? generateArtwork(`https://${track.cover}`) : undefined
-    }) : null;
+        artwork: cover
+    });
+
+    if (Notification.permission === "granted") {
+        if (lastNotification) lastNotification.close();
+
+        lastNotification = new Notification(title, {
+            silent: true,
+            body: artist || track.album?.title,
+            icon: cover ? cover[4].src : undefined
+        });
+
+        lastNotification.onclick = () => {
+            window.focus();
+        };
+    }
 }
+
+if (Notification.permission === "default") Notification.requestPermission();
+
+window.addEventListener("beforeunload", () => {
+    if (lastNotification) lastNotification.close();
+});
 
 externalAPI.on(externalAPI.EVENT_TRACK, () => updateTrack(externalAPI.getCurrentTrack()));
 
