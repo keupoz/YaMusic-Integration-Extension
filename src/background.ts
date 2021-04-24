912b12840302854831a6cea7176ba94286978dfb
Ivan Sokolov <keupoz@gmail.com>
2021-04-24 19:38:16 +0700

    Remove tslint ignore comment

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,6 @@ chrome.browserAction.onClicked.addListener(() => {
     chrome.tabs.query({ url: "https://music.yandex.ru/*" }, (result) => {
         const targetTab = result[0];
 
-        // tslint:disable-next-line: strict-type-predicates
         if (targetTab === undefined || targetTab.id === undefined) {
             chrome.tabs.create({
                 url: "https://music.yandex.ru/home"
